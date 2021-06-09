ab9dfbe70b80e6fad0bfeb4a698bf726fe853f64
Geonu Jeon <geonu1109@daou.co.kr>
2021-06-09 16:48:22 +0900

    Initial Commit

---
+++ Today/TodayApp.swift
@@ -0,0 +1,20 @@
+//
+//  TodayApp.swift
+//  Today
+//
+//  Created by Geonu Jeon on 2021/06/09.
+//
+
+import SwiftUI
+
+@main
+struct TodayApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
